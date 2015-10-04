e7da5ea4347a167941136ff46b70c2ead8333611
Dinis Lage <dinis@lage.pw>
2015-10-04 05:28:41 +0100

    Initial Spotify Kodi web auth platform. Spotify OAuth2 working.

---
+++ spotifykodiweb/settings/dev.py
@@ -0,0 +1,10 @@
+# Load defaults in order to then add/override with dev-only settings
+from defaults import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# Secret keys
+SECRET_KEY = 'd)cf686tjw_m%_h+e#@ghzzkn3%#w^9&v#@^2gpvmj@e9ea8%^'
+SOCIAL_AUTH_SPOTIFY_KEY = '7bd1ed9068ce4d46901b93074a7ee721'
+SOCIAL_AUTH_SPOTIFY_SECRET = 'ab482149aab140d1ad07e4c7605d2567'
